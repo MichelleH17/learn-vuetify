c5180688e1571529a51e4144db530437ad1bb4d8
Michaela Havlíková <Michelle.h@seznam.cz>
2024-10-17 23:16:38 +0200

    Create LoginView and add path to router

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,11 +5,14 @@
   <v-toolbar density="compact">
     <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-    <v-toolbar-title>Learn Vuetify 3</v-toolbar-title>
-
+    <v-toolbar-title>
+      <v-btn to="/" variant="plain">
+        Learn Vuetify 3
+      </v-btn>
+    </v-toolbar-title>
     <v-spacer></v-spacer>
 
-    <v-btn variant="outlined" color="blue-darken-2" size="small">Login
+    <v-btn to="/login" variant="outlined" color="blue-darken-2" size="small">Login
     </v-btn>
   </v-toolbar>
 </template>
