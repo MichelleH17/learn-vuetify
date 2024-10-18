5e654c6dbd2b57c34ea5c18ed7b4b66e569400e2
Michaela Havlíková <Michelle.h@seznam.cz>
2024-10-18 12:21:44 +0200

    Add notification icon

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,14 @@ import { RouterView } from 'vue-router'
           </v-btn>
         </v-toolbar-title>
         <v-spacer></v-spacer>
+
+        <v-btn icon class="mr-6">
+          <v-badge color="red-darken-2" content="5">
+            <v-icon icon="mdi-bell" color="blue-darken-3"></v-icon>
+          </v-badge>
+        </v-btn>
         
-        <v-btn to="/login" variant="outlined" color="blue-darken-2" size="small">Login
+        <v-btn to="/login" variant="outlined" color="blue-darken-3" size="small">Login
         </v-btn>
       </v-toolbar>
     </v-app-bar>
